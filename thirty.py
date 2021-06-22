2ab3e8a3513b6e46d06cd754a7785be6035dd0d3
Peter Andersson <fshsweden@gmail.com>
2021-06-22 23:24:21 +0200

    New version with advanced logic

--- thirty.py
+++ thirty.py
@@ -1,66 +1,153 @@
-def sumof2(arr, answer):
-    res = []
-    for x in arr:
-        for y in arr:
-            if x+y == answer:
-                res.append((x,y))
+from copy import *
 
-    return res
+def search_exact(arr, target):
+    tpl = None
+    for a in arr:
+        if a == target:
+            tpl = a
+            print("found a lonesome")
+            arr.remove(a)
+            break
+    return arr, tpl
 
-def sumof3(arr, answer):
-    res = []
-    for x in arr:
-        for y in arr:
-            for z in arr:
-                if x+y+z == answer:
-                    res.append((x,y,z))
+def search_2(arr, target):
+    tpl = None
 
-    return res
+    done = False
+    while not done and len(arr) >= 2:
 
-def sumof4(arr, answer):
-    res = []
-    for x in arr:
-        for y in arr:
-            for z in arr:
-                for a in arr:
-                    if x+y+z+a == answer:
-                        res.append((x,y,z,a))
+        # arr
 
-    return res
+        first = arr[0]
+        for x in arr[1:]:
+            if first + x == target:
+                print("found a 2some")
+                done = True
+                tpl = (first, x)
+                arr.remove(first)
+                arr.remove(x)
+                break
+
+        try:
+            if not done:
+                arr.remove(first)
+        except Exception as e:
+            pass
+
+    return arr, tpl
 
 
-def sumof5(arr, answer):
-    res = []
-    for x in arr:
-        for y in arr:
-            for z in arr:
-                for a in arr:
-                    for b in arr:
-                        if x+y+z+a+b == answer:
-                            res.append((x,y,z,a,b))
+def search_3(arr, target):
+    tpl = None
 
-    return res
+    done = False
+    while not done and len(arr) >= 3:
+        first = arr[0]
+        for x in arr[1:]:
+            if not done:
+                for y in arr[2:]:
+                    if first + x + y == target:
+                        print("found a 3some")
+                        done = True
+                        tpl = (first, x, y)
+                        arr.remove(first)
+                        arr.remove(x)
+                        arr.remove(y)
+                        break
+                try:
+                    if not done:
+                        arr.remove(first)
+                except Exception as e:
+                    pass
 
-def sumof6(arr, answer):
-    res = []
-    for x in arr:
-        for y in arr:
-            for z in arr:
-                for a in arr:
-                    for b in arr:
-                        for c in arr:
-                            if x+y+z+a+b+c == answer:
-                                res.append((x,y,z,a,b,c))
+    return arr, tpl
 
-    return res
 
-# 0 represents a "dice not counted"
 
-a = [0,1,2,3,4,5,6]
+def search(arr, target):
 
-print(set(sumof2(a, 5)))
-print(set(sumof3(a, 5)))
-print(set(sumof4(a, 5)))
-print(set(sumof5(a, 5)))
-print(set(sumof6(a, 5)))
+    orig = copy(arr)
+    res_of_1 = []
 
+    print("Searching 1")
+    # Stage 1
+    while True:
+        arr, tpl = search_exact(arr, target)
+        if tpl is None:
+            break
+        else:
+            res_of_1.append(tpl)
+
+    print("Searching 2")
+    res_of_2 = []
+    arr = copy(orig)
+    while True:
+        arr, tpl = search_2(arr, target)
+        if tpl is None:
+            break
+        else:
+            res_of_2.append(tpl)
+
+    print("Searching 3")
+    res_of_3 = []
+    arr = copy(orig)
+
+    while True:
+        arr, tpl = search_3(arr, target)
+        if tpl is None:
+            break
+        else:
+            res_of_3.append(tpl)
+
+    return res_of_1, res_of_2, res_of_3
+
+    # r3 = [(1,4),(1,4),(1,4)]
+
+
+#
+# Check if all parts of tuple still exists in array
+#
+def all_items_still_in_array(tpl, arr):
+    arr_cpy = copy(arr)
+    for tplitem in list(tpl):
+        try:
+            arr_cpy.remove(tplitem)
+        except:
+            return False
+    return True
+
+
+a = [1, 1, 4, 4, 1, 2]
+TARGET=6
+a = sorted(a)
+print(f"Original array: {a}")
+
+groups = search(a, TARGET)
+
+# DEBUG print list
+# groupnum = 1
+# for g in groups:
+#     print(f"group #{groupnum}:{g}")
+#     for x in list(g):
+#         print(f"Tuple-item in group is: {x}")
+#         for y in list(x):
+#             print(f"    Item in tuple is: {y}")
+#     groupnum += 1
+
+
+print("Counting points!")
+points = 0
+for g in groups:
+    for tpl in list(g):
+
+        if all_items_still_in_array(tpl, a):
+            print(f"YUP, counting this one: {tpl}")
+            points += TARGET
+            for item in list(tpl):
+                a.remove(item)
+        else:
+            print("NOPE, can't use this one")
+
+
+print(f"When target is {TARGET}, points = {points}")
+print(f"This is what is left of array: {a}")
