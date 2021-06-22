2d39bb1dcd41dc811a9aa711a4faf0d89c647639
David Andersson <davidandersson99@gmail.com>
2021-06-23 00:58:04 +0200

    fixed VERY buggy code and finished the algorithm

--- thirty.py
+++ thirty.py
@@ -6,7 +6,10 @@ def search_exact(arr, target):
         if a == target:
             tpl = a
             print("found a lonesome")
-            arr.remove(a)
+            try:
+                arr.remove(a)
+            except Exception as e:
+                pass
             break
     return arr, tpl
 
@@ -15,18 +18,19 @@ def search_2(arr, target):
 
     done = False
     while not done and len(arr) >= 2:
-
-        # arr
-
         first = arr[0]
         for x in arr[1:]:
-            if first + x == target:
-                print("found a 2some")
-                done = True
-                tpl = (first, x)
-                arr.remove(first)
-                arr.remove(x)
-                break
+            if not done:
+                if first + x == target:
+                    print("found a 2some in " + str(first) + " + " + str(x))
+                    done = True
+                    tpl = (first, x)
+                    try:
+                        arr.remove(first)
+                        arr.remove(x)
+                    except Exception as e:
+                        pass
+                    break
 
         try:
             if not done:
@@ -46,19 +50,126 @@ def search_3(arr, target):
         for x in arr[1:]:
             if not done:
                 for y in arr[2:]:
-                    if first + x + y == target:
-                        print("found a 3some")
-                        done = True
-                        tpl = (first, x, y)
-                        arr.remove(first)
-                        arr.remove(x)
-                        arr.remove(y)
-                        break
-                try:
                     if not done:
-                        arr.remove(first)
-                except Exception as e:
-                    pass
+                        if first + x + y == target:
+                            print("found a 3some")
+                            done = True
+                            tpl = (first, x, y)
+                            try:
+                                arr.remove(first)
+                                arr.remove(x)
+                                arr.remove(y)
+                            except Exception as e:
+                                pass
+                            
+                            break
+        try:
+            if not done:
+                arr.remove(first)
+        except Exception as e:
+            pass
+
+    return arr, tpl
+
+def search_4(arr, target):
+    tpl = None
+
+    done = False
+    while not done and len(arr) >= 4:
+        first = arr[0]
+        for x in arr[1:]:
+            if not done:
+                for y in arr[2:]:
+                    if not done:
+                        for z in arr[3:]:
+                            if first + x + y + z == target:
+                                print("found a 4some")
+                                done = True
+                                tpl = (first, x, y, z)
+                                try:
+                                    arr.remove(first)
+                                    arr.remove(x)
+                                    arr.remove(y)
+                                    arr.remove(z)
+                                except Exception as e:
+                                    pass
+                                break
+        try:
+            if not done:
+                arr.remove(first)
+        except Exception as e:
+            pass
+
+    return arr, tpl
+
+def search_5(arr, target):
+    tpl = None
+
+    done = False
+    while not done and len(arr) >= 5:
+        first = arr[0]
+        for x in arr[1:]:
+            if not done:
+                for y in arr[2:]:
+                    if not done:
+                        for z in arr[3:]:
+                            if not done:
+                                for i in arr[4:]:
+                                    if first + x + y + z + i == target:
+                                        print("found a 5some")
+                                        done = True
+                                        tpl = (first, x, y, z, i)
+                                        try:
+                                            arr.remove(first)
+                                            arr.remove(x)
+                                            arr.remove(y)
+                                            arr.remove(z)
+                                            arr.remove(i)
+                                        except Exception as e:
+                                            pass
+                                        break
+        try:
+            if not done:
+                arr.remove(first)
+        except Exception as e:
+            pass
+
+    return arr, tpl
+
+def search_6(arr, target):
+    tpl = None
+
+    done = False
+    while not done and len(arr) >= 6:
+        first = arr[0]
+        for x in arr[1:]:
+            if not done:
+                for y in arr[2:]:
+                    if not done:
+                        for z in arr[3:]:
+                            if not done:
+                                for i in arr[4:]:
+                                    if not done:
+                                        for p in arr[5:]:
+                                            if first + x + y + z + i + p == target:
+                                                print("found a 6some")
+                                                done = True
+                                                tpl = (first, x, y, z, i, p)
+                                                try:
+                                                    arr.remove(first)
+                                                    arr.remove(x)
+                                                    arr.remove(y)
+                                                    arr.remove(z)
+                                                    arr.remove(i)
+                                                    arr.remove(p)
+                                                except Exception as e:
+                                                    pass
+                                                break
+        try:
+            if not done:
+                arr.remove(first)
+        except Exception as e:
+            pass
 
     return arr, tpl
 
@@ -99,7 +210,40 @@ def search(arr, target):
         else:
             res_of_3.append(tpl)
 
-    return res_of_1, res_of_2, res_of_3
+    print("Searching 4")
+    res_of_4 = []
+    arr = copy(orig)
+
+    while True:
+        arr, tpl = search_4(arr, target)
+        if tpl is None:
+            break
+        else:
+            res_of_4.append(tpl)
+
+    print("Searching 5")
+    res_of_5 = []
+    arr = copy(orig)
+
+    while True:
+        arr, tpl = search_5(arr, target)
+        if tpl is None:
+            break
+        else:
+            res_of_5.append(tpl)
+
+    print("Searching 6")
+    res_of_6 = []
+    arr = copy(orig)
+
+    while True:
+        arr, tpl = search_6(arr, target)
+        if tpl is None:
+            break
+        else:
+            res_of_6.append(tpl)
+
+    return res_of_1, res_of_2, res_of_3, res_of_4, res_of_5, res_of_6
 
     # r3 = [(1,4),(1,4),(1,4)]
 
@@ -117,8 +261,8 @@ def all_items_still_in_array(tpl, arr):
     return True
 
 
-a = [1, 1, 4, 4, 1, 2]
-TARGET=6
+a = [1, 1, 1, 2, 4, 4]
+TARGET=5
 a = sorted(a)
 print(f"Original array: {a}")
 
